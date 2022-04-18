48dcd8fbdc418b715730628a0e5f9c64676a17fc
Yannick Houbrix <sh33dafi@users.noreply.github.com>
2022-04-18 19:21:18 +0200

    initial commit

---
+++ apps/darts-score/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'darts-score-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'darts-score';
+}
